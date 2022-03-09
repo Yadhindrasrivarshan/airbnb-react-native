2f1f9ae5c7cc51ee855fdb5351e3d619a8da3278
YadhindraSrivarshan <yadhindrasrivarshan_pa@trimble.com>
2022-03-09 09:54:39 +0530

    HomeScreen done

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import EnType from 'react-native-vector-icons/Entypo';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import HomeScreen from './src/screens/Home';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <HomeScreen />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
